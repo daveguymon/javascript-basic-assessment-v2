57ba1ddf0e025cbbd31366f013102ea29648ceeb
Dave Guymon <davidwguymon@gmail.com>
2017-06-30 11:25:05 -0600

    Completed assessment with 0 failures.

--- assessment.js
+++ assessment.js
@@ -1,9 +1,11 @@
 // #1 Create a var called 'up' that says 'down'
 
-
+var up = 'down';
 
 // #2 Use the var iHeart to create a new var called iHeartJavascript that says 'I heart javascript'
 var iHeart = 'I heart';
+
+var iHeartJavascript = iHeart + " javascript";
 
 // #3  Make an object called developer
 // developer has a title of Junior (String)
@@ -11,7 +13,16 @@ var iHeart = 'I heart';
 // developer has a laptop, that has type Macbook, and year 2016 (object)
 // developer is not billGates (boolean)
 // developer likes isolation, caffeine, and pizza (array of strings)
-
+var developer = {
+    title: 'Junior',
+    salary: 55000,
+    laptop: {
+        type: 'Macbook',
+        year: 2016
+    },
+    billGates: false,
+    likes: ['isolation', 'caffeine', 'pizza']
+}
 
 // #4 Change my dog's name to Fido using dot notation
 var myDog = {
@@ -19,15 +30,28 @@ var myDog = {
 	name: 'Spot'
 };
 
+myDog.name = 'Fido';
+
 // Change my don't type to hairless using square bracket notation
 var myOtherDog = {
 	type: 'poodle',
 	name: 'Fluffy'
 };
 
+myOtherDog['type'] = 'hairless';
+
 // #5 Create an object that tracks a count of kids in each grade in a school.  Call it 'school'
 // The key should be the grade (string) and the value should be how many kids there are.
 // Our school has 50 kids in first, 30 kids in second and 80 kids in third
+
+
+var school = {
+    first: 50,
+    second: 30,
+    third: 80
+}
+
+
 
 
 
@@ -41,19 +65,38 @@ var briefing = {
 };
 
 
+for(var prop in briefing) {
+    if(prop.startsWith('secret')){
+        briefing[prop] = 'redacted';
+    }
+}
+
+
 // #7 Create an array of strings that are the 7 days of the week. Call your array daysOfWeek
+
+var daysOfWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
 
 // #8 Using this array do the following
 var jedi = ['Yoda', 'Mace Windu', 'Obi-Wan'];
 // add 'Luke' to the end
 
+jedi.push('Luke');
+
 // remove 'Yoda' and store him in a var called firstJedi
 
+var firstJedi = jedi.shift();
+
 // add 'Mario' to the start of the array
+jedi.unshift('Mario');
 
 // remove 'Obi-Wan' from the array and store him in a var called secondJedi
+var secondJedi = jedi.splice(2,1).toString();
+
 
 // leave 'Mace Windu' in the array but put a copy of him on a var called thirdJedi
+var thirdJedi = jedi.slice(1,2).toString();
+
 
 
 
@@ -62,25 +105,81 @@ var jedi = ['Yoda', 'Mace Windu', 'Obi-Wan'];
 // after the first item in the array and return the array
 // EX: cutInLine([1,2,3,4], 8) => [1,8,2,3,4];
 
+function cutInLine(arr, item) {
+    var solution = arr.splice(1, 0, item);
+    return arr;
+}
+
 
 // #10 Write a function called removeThings, that takes in an array of things (strings or numbers), and a 'thing' (a string or a number).
 // Removes all instances of that 'thing' from the array. And return the modified array.
 // The order of the array should not be changed
 // removeThings(['a', 1, 4, 'a'], 'a') => [1,4]
 
+function removeThings(arr, thing) {
+    var solution = arr.filter(function(val) {
+      return val !== thing;
+    })
+    return solution;
+}
+
 
 // #11 Write a function tripleTheFun that takes 1 parameter: a number or a string. It triples numbers, and
 // repeats strings. Example: 4->12, 2.5->7.5, 'Awesome'->'AwesomeAwesomeAwesome'
-
+function tripleTheFun(thing) {
+  
+    var num = Number(thing)
+    if(Number.isNaN(num)) {
+      return thing+thing+thing;
+    }
+    else {
+      return thing* 3;
+    }
+  
+}
 
 // #12 Write function getPropertyValue that takes in an object, and the name of a property on the object as a string
 // return the value from the object that corresponds to the property
+
+function getPropertyValue(obj, name) {
+    return obj[name];
+}
 
 // #13 Write a function called clockIn that takes in an employee and department name
 // and returns an object with a name, department, and timestamp which is
 // the current time as a Date object
 
+function clockIn(employee, department) {
+    var obj = {
+        name: employee,
+        department: department,
+        timestamp: new Date()
+    }
+    return obj;
+}
+
+
+
+
 // #14 Create a function called spiderTest that takes in an object that is an actor, it looks to see if the actor's name is Andrew Garfield and changes the actor object to have a property called spiderRating with a value of 10.  If their name is Tobey Maguire set spiderRating to 0.  otherwise set spiderRating to 'not a spiderman'.
+
+
+function spiderTest(actorObj, name) {
+    for (var key in actorObj) {
+        if(actorObj[key] === "Andrew Garfield") {
+            actorObj.spiderRating = 10;
+        }
+        else if(actorObj[key] === "Tobey Maguire") {
+            actorObj.spiderRating = 0;
+        }
+        else {
+            actorObj.spiderRating = 'not a spiderman';
+        }
+    }
+    
+}
+
+
 
 // #15 Create a function called whatIsItOutside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
 /*
@@ -91,5 +190,28 @@ var jedi = ['Yoda', 'Mace Windu', 'Obi-Wan'];
     Otherwise - return "I love outside"
 */
 
+function whatIsItOutside(temp, hum, cloud) {
+  if(temp > 85 && hum > 45) {
+    return "I'm all sweat";
+  } 
+  else if (temp < 40 && cloud > 60){
+    return "I have icicles";
+  }
+  else if (temp > 85 && hum < 45 && cloud < 20) {
+    return "I'm literally in the desert";
+  }
+  else if (temp > 90 || hum > 55 || cloud > 65) {
+    return "Hmm, probably not";
+  }
+  else {
+    return "I love outside";
+  }
+  
+}
+
 // #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the string(back) + ' back'.
 // IE - If I call you with 'Give it' you should invoke holla with 'Give it back'
+
+function callerBack(cb, str) {
+    cb(str + ' back');
+}
